d98c973b60bbe0486f81c68ba6a24c3465b467f4
wanggen <wanggen6833@163.com>
2018-12-16 19:35:09 +0800

    添加test工程到git

---
+++ test-git/src/cn/itcast/TestGit.java
@@ -0,0 +1,13 @@
+package cn.itcast;
+
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
